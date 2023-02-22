77065c0dd0bbbead6b4d5abead5a41f0ba6ca1b0
JINUK JEONG <wlsdnr214@gmail.com>
2023-02-22 23:48:51 +0900

    2023-02-22 algorithm study

--- python_study/D2/5174.py
+++ python_study/D2/5174.py
@@ -1,7 +1,7 @@
 def preorder(T):
-    if T == 0:
+    if T == 0: # tree[0][0], tree[1][0]는 트리에 없는 값
         return 0
-    return 1 + preorder(tree[T][0]) + preorder(tree[T][1])
+    return 1 + preorder(tree[0][T]) + preorder(tree[1][T])
     # 재귀할 때마다 +1 왼쪽과 오른쪽 노드 탐색
 
 T = int(input())
@@ -9,13 +9,14 @@ for t in range(1, T + 1):
     E, N = map(int, input().split())  # N을 루트로 하는 서브 트리에 속한 노드 개수 E
     data = list(map(int, input().split()))  #
 
-    tree = [[0] * 2 for _ in range(E + 2)]
+    tree = [[0] * (E + 2) for _ in range(2)]
 
     for i in range(0, E * 2, 2):  # 입력받은 간선 정보를 트리에 넣어줌
-        if not tree[data[i]][0]:  # 왼쪽이 비어있다면
-            tree[data[i]][0] = data[i + 1]
+        if not tree[0][data[i]]:  # 왼쪽이 비어있다면
+            tree[0][data[i]] = data[i + 1]
         else:  # 왼쪽이 있다면 오른쪽
-            tree[data[i]][1] = data[i + 1]
+            tree[1][data[i]] = data[i + 1]
 
+    print(tree)
     ans = preorder(N)  # 루트로 받아주는 N부터 찾음
     print(f'#{t} {ans}')
--- python_study/D4/1231.py
+++ python_study/D4/1231.py
@@ -1,25 +1,24 @@
 def inorder(T):  # 중위순회
     if chars[T] == '': # 문자 없으면 반환
         return
-    inorder(tree_left[T]) # 왼쪽 먼저
+    inorder(tree[0][T]) # 왼쪽 먼저
     print(chars[T], end='')
-    inorder(tree_right[T]) # 그다움 오른쪽
+    inorder(tree[1][T]) # 그다움 오른쪽
 
 
 for t in range(1, 11):
     N = int(input())
 
     chars = [''] * (N + 1) # 문자 담는 곳
-    tree_left = [0] * (N + 1) # 트리 왼쪽
-    tree_right = [0] * (N + 1) # 트리 오른쪽
+    tree = [[0] * (N + 1) for _ in range(2)] # 트리만들기
     for _ in range(N):
         data = input().split() # 입력 받기
         num = int(data[0])
         chars[num] = data[1] # 각 노드에 문자 넣어주기
         if len(data) > 2: # 왼쪽트리 입력받을 경우
-            tree_left[num] = int(data[2])
+            tree[0][num] = int(data[2])
         if len(data) > 3: # 오른쪽트리도 입력받는 경우
-            tree_right[num] = int(data[3])
+            tree[1][num] = int(data[3])
 
     print(f'#{t}', end=' ')
     inorder(1)
